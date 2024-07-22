6105a4b5aa39f8a124a061c99b686b5a6220fa68
Priyanshu Mehta <pmehta305@gatech.edu>
2024-07-21 22:11:53 -0400

    rag.py -> embdedings.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from src.rag.core.rag import create_vector_db
+from rag.core.embeddings import create_vector_db
 from src.rag.core.query import query
 
-create_vector_db()
+# create_vector_db()
 query("Give me a detailed summary of this?")
